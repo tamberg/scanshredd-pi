c024364450621ca1398356d2a6fcc70d85ce131f
tamberg <tamberg@yaler.net>
2023-08-28 10:25:48 +0200

    Update.

--- scanshredd.js
+++ scanshredd.js
@@ -3,9 +3,33 @@
 const gpio = require("rpi-gpio");
 const RaspiCam = require("raspicam");
 
+// based on code by Peter Auchli, license = ? >>
+const fs = require('fs');
+const request = require('request').defaults({ maxRedirects: 4 });
+
+function savePhotoOnline(f) {
+    var formData = {
+        upfile: fs.createReadStream("/home/pi/photo.png"),
+    };
+    var x = {
+        url: 'https://partydesjahres.ch/nichts/photoboothupload.php',
+        formData: formData
+    }
+    request.post(x, (err, httpResponse, body) => {
+        if (err) {
+            console.error('Request failed with:', err);
+        } else {
+            console.log('Server responded with:', body);
+        }
+        f();
+    });
+}
+// <<
+
 const cam = new RaspiCam({
     mode: "photo",
-    output: "./photo.jpg"
+    output: "./photo.png",
+    e: "png"
 });
 
 const btn_pin = 5;
@@ -22,7 +46,7 @@ cam.on("start", () => {
 
 cam.on("read", (err, timestamp, filename) => {
     console.log("cam read " + filename + ", t = " + timestamp + " s.");
-    gpio.write(led_pin, false);
+    savePhotoOnline(() => { gpio.write(led_pin, false); });
 });
 
 gpio.on("change", (pin, value) => {
