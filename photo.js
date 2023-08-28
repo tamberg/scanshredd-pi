a964cf1b9b8e9df219fa432a6e8db366f6383c1e
tamberg <tamberg@yaler.net>
2023-08-28 12:53:34 +0200

    Update.

--- photo.js
+++ photo.js
@@ -5,7 +5,10 @@ const RaspiCam = require("raspicam");
 const cam = new RaspiCam({
     mode: "photo",
     output: "./photo.png",
-    e: "png"
+    width: 1920,
+    height: 1080,
+    rotation: 90,
+    encoding: "png"
 });
 
 cam.start();
